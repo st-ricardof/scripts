72fa2101cfa9cf73866c02d73f705b888b1920cf
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-03-26 23:22:05 -0300

    script próaluno ok

--- 2019/analisa-logs-proaluno/script.R
+++ 2019/analisa-logs-proaluno/script.R
@@ -1,8 +1,9 @@
-data <- read.csv("~/Downloads/log1.csv", row.names = NULL,nrows=100000, stringsAsFactors= FALSE)
+data <- read.csv("~/Downloads/log1.csv", row.names = NULL,nrows=1000000, stringsAsFactors= FALSE)
 
 dates<-c()
 accounts<-c()
 address<-c()
+tipo<-c()
 
 for(i in seq(1:nrow(data))) {
 
@@ -10,28 +11,33 @@ for(i in seq(1:nrow(data))) {
   
   col6array = strsplit(data[i,6], split='\n')[[1]]
   
-  if(grepl("Nome da Conta:",data[i,6])){
-    accountsArray = col6array[grep("Nome da Conta:",col6array)]
+  if(grepl("Nome da conta:",data[i,6])){
+    accountsArray = col6array[grep("Nome da conta:",col6array)]
     accounts_string = paste(accountsArray, sep = " ", collapse = " - ")
     accounts = append(accounts,accounts_string)
     dates = append(dates,data[i,2])
+    tipo = append(tipo,data[i,5])
   } else {
-    accounts = append(accounts," - ")
+    accounts = append(accounts,"-")
     dates = append(dates,data[i,2])
+    tipo = append(tipo,data[i,5])
   }
   
-  if(grepl("Endereço da Rede",data[i,6])){
-    addressArray = col6array[grep("Endereço da Rede",col6array)]
+  if(grepl("Endereço do cliente",data[i,6])){
+    addressArray = col6array[grep("Endereço do cliente",col6array)]
     addressString = paste(addressArray, sep = " ", collapse = " - ")
     address = append(address,addressString)
   } else {
-    address = append(address," - ")
+    address = append(address,"-")
   }
-
 }
 
-output <- data.frame(dates,accounts,address)
+output <- data.frame(dates,tipo,accounts,address)
 
-#output <- output[ which(output[,2]!=NULL & output[,3]!=NULL), ]
+output <- output[ which(output[,3]!='-' & output[,4]!='-'), ]
+
+# Exclude PAFCS-
+
+output <- output[ !grepl("PAFCS-",output[,3]), ]
 
 write.csv(file = "~/output.csv",output)
