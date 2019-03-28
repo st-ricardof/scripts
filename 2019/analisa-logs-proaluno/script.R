9b21932ffdf9bb866108940d5d2a63af84c716a3
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-03-28 10:44:36 -0300

    limpando output

--- 2019/analisa-logs-proaluno/script.R
+++ 2019/analisa-logs-proaluno/script.R
@@ -1,9 +1,10 @@
-data <- read.csv("~/Downloads/log1.csv", row.names = NULL, stringsAsFactors= FALSE)
+data <- read.csv("~/Downloads/SEG.csv", row.names = NULL, stringsAsFactors= FALSE)
+
+Encoding(data[,6]) <- "UTF-8"
 
 dates<-c()
 accounts<-c()
 address<-c()
-tipo<-c()
 
 for(i in seq(1:nrow(data))) {
 
@@ -14,30 +15,40 @@ for(i in seq(1:nrow(data))) {
   if(grepl("Nome da conta:",data[i,6])){
     accountsArray = col6array[grep("Nome da conta:",col6array)]
     accounts_string = paste(accountsArray, sep = " ", collapse = " - ")
+
+    accounts_string = sub("Nome da conta:","", accounts_string)
+    accounts_string = sub("@PAFFLCH.FFLCH.USP.BR","", accounts_string)
+
     accounts = append(accounts,accounts_string)
     dates = append(dates,data[i,2])
-    tipo = append(tipo,data[i,5])
   } else {
     accounts = append(accounts,"-")
     dates = append(dates,data[i,2])
-    tipo = append(tipo,data[i,5])
   }
   
   if(grepl("Endereço do cliente",data[i,6])){
     addressArray = col6array[grep("Endereço do cliente",col6array)]
     addressString = paste(addressArray, sep = " ", collapse = " - ")
+
+    addressString = gsub("Endereço do cliente:","",addressString)
+    addressString = gsub("::ffff:","",addressString)
+    
     address = append(address,addressString)
   } else {
     address = append(address,"-")
   }
 }
 
-output <- data.frame(dates,tipo,accounts,address)
-
-output <- output[ which(output[,3]!='-' & output[,4]!='-'), ]
+output <- data.frame(dates,accounts,address)
+output <- output[ which(output[,2]!='-' & output[,3]!='-'), ]
 
 # Exclude PAFCS-
+output <- output[ !grepl("PAFCS-",output[,2]), ]
+output<-as.data.frame(sapply(output,trimws))
 
-output <- output[ !grepl("PAFCS-",output[,3]), ]
+# Pessoas que mais fizeram login:
+summary(output$accounts)
+
+output[ which(output[,2]=='pa-teste'), ]
 
 write.csv(file = "~/output.csv",output)
