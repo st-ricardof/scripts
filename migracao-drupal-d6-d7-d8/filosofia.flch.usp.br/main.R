460f9735add7643a795e515f763eea9c042216c7
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-04-22 19:39:18 -0300

    filosofia bolsas pós ok

--- migracao-drupal-d6-d7-d8/filosofia.flch.usp.br/main.R
+++ migracao-drupal-d6-d7-d8/filosofia.flch.usp.br/main.R
@@ -27,7 +27,7 @@ df$field_data = df$field_data + 3*60*60
 # converte para string no formato "2020-03-31T13:00:00"
 df$field_data = str_replace(as.character(df$field_data), ' ', 'T')
 
-write.csv(df,"outputs/agenda_de_defesas.csv",row.names = F)
+write.csv(df,"outputs/agenda_de_defesas.csv",row.names = F,na = "")
 
 #########################
 # Aviso (Identificador: aviso)
@@ -46,7 +46,7 @@ colnames(df)[names(df) == "field_corpo"] = 'field_corpo|value'
 df$field_data_expira_home = strptime(df$field_data_expira_home, "%d/%m/%Y")
 df$field_data_expira_home = as.character(df$field_data_expira_home)
 
-write.csv(df,"outputs/avisos.csv",row.names = F)
+write.csv(df,"outputs/avisos.csv",row.names = F,na = "")
 
 #################################
 # Informes (Identificador: pagina_inicial)
@@ -61,7 +61,7 @@ df <- data.frame(lapply(df, trimws), stringsAsFactors = FALSE)
 df['body|format']='full_html'
 colnames(df)[names(df) == "body"] = 'body|value'
 
-write.csv(df,"outputs/informes.csv",row.names = F)
+write.csv(df,"outputs/informes.csv",row.names = F,na = "")
 
 ###########################
 # Agenda de Eventos (Identificador: eventos)
@@ -102,29 +102,53 @@ df$field_data_eventos_externos_fim = str_replace(as.character(df$field_data_even
 colnames(df)[names(df) == "field_data_eventos_externos"] = 'field_data_eventos_externos|value'
 colnames(df)[names(df) == "field_data_eventos_externos_fim"] = 'field_data_eventos_externos|end_value'
 
-#write.csv(df[,c(1,2,3,4,5,6,7,8)],
-#          "outputs/agenda_de_eventos.csv",row.names = F)
+write.csv(df,"outputs/agenda_de_eventos.csv",row.names = F,na = "")
 
-write.csv(df,"outputs/agenda_de_eventos.csv",row.names = F)
-
-#############
-
-
+############################
 # Bolsas Pós-graduação (Identificador: bolsas) - sem títulos
+# full_html: field_corpo_bolsas, field_renovacoes, field_corpo_tabelas
+# data: field_pervig_inicio e field_pervig_fim
+df = read.csv("./inputs/bolsas_pos.csv", stringsAsFactors = F)
+nrow(df)
+
+# trim nas colunas:
+df <- data.frame(lapply(df, trimws), stringsAsFactors = FALSE)
+
+df['field_corpo_bolsas|format']='full_html'
+colnames(df)[names(df) == "field_corpo_bolsas"] = 'field_corpo_bolsas|value'
+
+df['field_renovacoes|format']='full_html'
+colnames(df)[names(df) == "field_renovacoes"] = 'field_renovacoes|value'
+
+df['field_corpo_tabelas|format']='full_html'
+colnames(df)[names(df) == "field_corpo_tabelas"] = 'field_corpo_tabelas|value'
+
+# field_data_do_evento e field_data_do_evento_fim: intervalo de datas
+df$field_pervig_inicio = strptime(paste(df$field_pervig_inicio, "02", sep="/"), "%m/%Y/%d")
+df$field_pervig_fim = strptime(paste(df$field_pervig_fim, "02", sep="/"), "%m/%Y/%d")
+
+# converte para string no formato "2020-03-31T13:00:00"
+df$field_pervig_inicio = paste(df$field_pervig_inicio, "T13:00:00", sep="")
+df$field_pervig_fim = paste(df$field_pervig_fim, "T13:00:00", sep="")
+colnames(df)[names(df) == "field_pervig_inicio"] = 'field_pervig|value'
+colnames(df)[names(df) == "field_pervig_fim"] = 'field_pervig|end_value'
+write.csv(df,"outputs/bolsas_pos.csv",row.names = F, na = "")
+
+
+###################
 # Docentes (Identificador: professores) - sem títulos
 # Editais e Oportunidades (Identificador: editais_e_oportunidades)
 # Grade horária e ementas (Identificador: grade_horaria)
-
 # Processos seletivos de Pós-graduação (Identificador: processo_seletivo_de_p_s_gradua_)
 # Publicações (Identificador: lan_amentos)
-# Página (Identificador: page)
-#   No tipo de conteúdo página é usado o módulo field_jquery_tabs
-#   Portanto os campos relacionado as tabs não foram migrados
-#   Migrar anexos
 # Pós-doutorado (Identificador: posdoutorado)
 # Teses e dissertações defendidas (Identificador: teses_e_dissertacoes_defendidas)
 # Vídeos (Identificador: video)
 
+# Página (Identificador: page)
+#   No tipo de conteúdo página é usado o módulo field_jquery_tabs
+#   Portanto os campos relacionado as tabs não foram migrados
+#   Migrar anexos
 
 
 # Módulos pós-migração
